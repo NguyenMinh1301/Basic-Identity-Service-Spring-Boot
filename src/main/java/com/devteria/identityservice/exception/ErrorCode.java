1be93d7b0c3d36ec7c31089c648411c83cd2a407
NguyenMinh1301 <nguyenminhjqka@gmail.com>
2025-04-21 20:57:55 +0700

    add exception for username and password

--- src/main/java/com/devteria/identityservice/exception/ErrorCode.java
+++ src/main/java/com/devteria/identityservice/exception/ErrorCode.java
@@ -2,7 +2,9 @@ package com.devteria.identityservice.exception;
 
 public enum ErrorCode {
     UNCATEGORIZED_EXCEPTION(4444, "Uncategorzed Exception"),
-    USER_EXISTED(1001, "User existed");
+    USER_EXISTED(1001, "User existed"),
+    USERNAME_INVALID(1002, "Username must be at least 3 characters"),
+    PASSWORD_INVALID(1003, "Password must be at least 8 characters");
 
     ErrorCode(int code, String message) {
         this.code = code;
