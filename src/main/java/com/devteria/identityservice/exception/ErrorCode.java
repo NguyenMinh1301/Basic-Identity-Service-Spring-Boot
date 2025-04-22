f540b2d3de68a5e2da6afe53764505a1bf21769e
NguyenMinh1301 <nguyenminhjqka@gmail.com>
2025-04-22 15:37:52 +0700

    Add Bcrypt and log-in function

--- src/main/java/com/devteria/identityservice/exception/ErrorCode.java
+++ src/main/java/com/devteria/identityservice/exception/ErrorCode.java
@@ -4,8 +4,9 @@ public enum ErrorCode {
     UNCATEGORIZED_EXCEPTION(4444, "Uncategorzed Exception"),
     INVALID_KEY(6666, "Invalid Message Key"),
     USER_EXISTED(1001, "User existed"),
-    USERNAME_INVALID(1002, "Username must be at least 3 characters"),
-    PASSWORD_INVALID(1003, "Password must be at least 8 characters");
+    USER_NOT_EXISTED(1002, "User not existed"),
+    USERNAME_INVALID(1003, "Username must be at least 3 characters"),
+    PASSWORD_INVALID(1004, "Password must be at least 8 characters");
 
     ErrorCode(int code, String message) {
         this.code = code;
